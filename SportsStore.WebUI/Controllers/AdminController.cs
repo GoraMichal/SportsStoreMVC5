f325c881a244e756c7aca671c55396c16275b97d
Michał Góra <michalgorazs3@gmail.com>
2020-03-28 21:19:29 +0100

    Update products

--- SportsStore.WebUI/Controllers/AdminController.cs
+++ SportsStore.WebUI/Controllers/AdminController.cs
@@ -29,5 +29,22 @@ namespace SportsStore.WebUI.Controllers
             Product product = repository.Products.FirstOrDefault(p => p.ProductID == productId);
             return View(product);
         }
+
+        [HttpPost]
+        public ActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                repository.SaveProduct(product);
+                TempData["message"] = string.Format("Zapisano {0} ", product.Name);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                // błąd w wartościach danych
+                return View(product);
+            }
+        }
+
     }
 }
